f33a1eac8bbe10b0443be556bd4d58ec610caf0d
fajjarnr <zfajart@gmail.com>
2021-12-09 12:26:05 +0700

    layout

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import Nav from './Nav';
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+      <Nav />
+      <main>{children}</main>
+    </div>
+  );
+}
