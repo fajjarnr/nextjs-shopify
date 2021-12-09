b9462bcf25387ebddebe9daf36a05b374e906ba6
fajjarnr <zfajart@gmail.com>
2021-12-09 22:39:06 +0700

    hero

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Footer from './Footer';
+import Hero from './Hero';
 import Nav from './Nav';
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
+      <Hero />
       <main>{children}</main>
       <Footer />
     </div>
