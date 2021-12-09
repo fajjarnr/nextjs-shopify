8c060c589455ac61e3cd75d81d535ad43f7ca4d2
fajjarnr <zfajart@gmail.com>
2021-12-09 22:41:37 +0700

    hero

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,10 @@
 import Footer from './Footer';
-import Hero from './Hero';
 import Nav from './Nav';
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
-      <Hero />
       <main>{children}</main>
       <Footer />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { getProductsInCollection } from '../lib/shopify';
 import ProductList from '../components/ProductList';
+import Hero from '../components/Hero';
 
 export default function Home({ products }) {
   return (
@@ -9,7 +10,7 @@ export default function Home({ products }) {
         <title>Jual Beli</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Hero />
       <ProductList products={products} />
     </div>
   );
