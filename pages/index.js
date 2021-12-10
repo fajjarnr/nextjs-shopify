3cea3d1edb941eb2bd4e567560d68bcc09b94e8d
fajjarnr <zfajart@gmail.com>
2021-12-10 15:20:17 +0700

    seo

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,30 @@ export default function Home({ products }) {
       <Head>
         <title>Jual Beli</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        />
+        <meta
+          name="description"
+          content="Modern eCommerce focusing on Shopify, Next.js, TailwindCSS, GraphQL."
+        />
+        <meta property="og:title" content="Jual Beli" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jual-beli.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://jual-beli.vercel.app/images/share.png"
+        />
+        <meta
+          property="og:description"
+          content="Modern eCommerce focusing on Shopify, Next.js, TailwindCSS, GraphQL."
+        />
+        <meta property="og:determiner" content="the" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Jual Beli" />
       </Head>
       <Hero />
       <ProductList products={products} />
